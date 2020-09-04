eb7fb42d555d4f141842ccddc4f740dbf779c4ac
Femi Akinyemi <akinfemi46@gmail.com>
2020-09-04 13:53:02 +0100

    update

--- src/Components/Language.js
+++ src/Components/Language.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
  class Language extends Component {
   render() {
-      console.log(this.props.name)
+     
     return (
         <div className='language-item'>
-        <div className='language-name'>{this.props.name} </div>
+            <div> <img  src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg"></img>   </div>
+        {/* <div className='language-name'>{this.props.name} </div> */}
         
         <img className='language-image' src={this.props.image} alt=""/>
         {/* <img className='language-image' src='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg' alt=""/> */}
